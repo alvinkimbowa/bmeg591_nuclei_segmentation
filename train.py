9bc219b5dc10aa04db0f96497eb51554eb29ec47
Alvin <kimbowa2alvin@gmail.com>
2025-03-05 09:07:25 -0800

    feat: add shorthand options for batch size and epochs in training script

--- train.py
+++ train.py
@@ -19,8 +19,8 @@ def arguments_parser():
     parser = argparse.ArgumentParser(description="Training script for segmentation")
     # Adding arguments for command line execution
     parser.add_argument('--data_dir', type=str, default='/home/ultrai/datasets/NuInsSeg', help='Path to the dataset')
-    parser.add_argument('--batch_size', type=int, default=1, help='Batch size for training and evaluation')
-    parser.add_argument('--epochs', type=int, default=10, help='Number of epochs to train')
+    parser.add_argument('-b', '--batch_size', type=int, default=1, help='Batch size for training and evaluation')
+    parser.add_argument('-e', '--epochs', type=int, default=10, help='Number of epochs to train')
     parser.add_argument('--lr', type=float, default=1e-3, help='Learning rate for the optimizer')
     parser.add_argument('--val_size', type=float, default=0.2, help='Fraction of data to use for validation')
     parser.add_argument('--test_size', type=float, default=0.1, help='Fraction of data to use for testing')
